dfd16cde0c76c7f84bb1828a69aaef7f818285e3
MikeB <burton.mj@gmail.com>
2017-01-17 13:53:51 -0700

    changes to factorial.js

--- js/basic/factorial.js
+++ js/basic/factorial.js
@@ -1,3 +1,12 @@
 // write a function that that takes a number and returns
 // its factorial. example:
 // `factorial(4)` // => 24 (4 * 3 * 2 * 1 = 24)
+
+function myFactorial(num){
+  var counter = 1;
+  for (var i = num; i > 0; i--){
+    counter *= i;
+  } return counter;
+}
+
+console.log(myFactorial(6));
